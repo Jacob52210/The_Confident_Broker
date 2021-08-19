873f9cd4773f8636e1b088b1e160545411ef9da7
Jacob Flora <jacobflora4@gmail.com>
2021-08-19 18:05:46 -0400

    changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Greeting from './components/landingpage';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results-page';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="https://jacob52210.github.io/The_Confident_Broker/" exact component={Greeting} />
-          <Route path="https://jacob52210.github.io/The_Confident_Broker/form" component={Form} />
-          <Route path="https://jacob52210.github.io/The_Confident_Broker/results" component={Results} />
+          <Route path="/" exact component={Greeting} />
+          <Route path="/form" component={Form} />
+          <Route path="/results" component={Results} />
         </Switch>
         <Footer />
       </div>
--- src/components/results-page.jsx
+++ src/components/results-page.jsx
@@ -5,7 +5,7 @@ function Results() {
   return (
     <div className="form-container">
       <h3>These are the Results.</h3>
-      <a href="https://jacob52210.github.io/The_Confident_Broker/" className="btn">Home</a>
+      <a href="/" className="btn">Home</a>
     </div>
   );
 }
