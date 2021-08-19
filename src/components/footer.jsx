d49fcde07878efb5f0552fe49a5b1308798c2cde
Jacob Flora <jacobflora4@gmail.com>
2021-08-19 16:57:36 -0400

    deploy my app to Github Pages

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="text">Copywrite 2021</p>
+      <p className="text">Pivacy Policy</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/form.jsx
@@ -0,0 +1,16 @@
+import './form.css';
+
+
+function Form() {
+  return (
+    <div className="form-container">
+      <h3>This is the Form.</h3>
+      <div className="btn-container">
+        <a className="btn" href="/results">Submit</a>
+        <a className="btn" href="/form">Clear</a>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
